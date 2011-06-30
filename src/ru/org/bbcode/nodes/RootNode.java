555abc40f9286f6f9f6953bc08814d5d5f1db0c4
Ildar Hizbulin <hizel@vyborg.ru>
2011-06-30 15:38:07 +0400

    все Tags

---
+++ src/ru/org/bbcode/nodes/RootNode.java
@@ -0,0 +1,11 @@
+package ru.org.bbcode.nodes;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hizel
+ * Date: 6/30/11
+ * Time: 3:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RootNode {
+}
---
+++ src/ru/org/bbcode/nodes/TagNode.java
@@ -0,0 +1,11 @@
+package ru.org.bbcode.nodes;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hizel
+ * Date: 6/30/11
+ * Time: 3:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TagNode {
+}
